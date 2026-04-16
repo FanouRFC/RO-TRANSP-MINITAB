328c419aec23a5ff4e289004543ac574c3499cfa
unknown <fanouomega@gmail.com>
2026-04-16 23:51:40 +0300

    initialisation

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-center text-4xl font-bold">
+        Projet RO : TRANSP - MINITAB - STEPPING STONE
+      </p>
+    </>
+  );
+}
+
+export default App;
