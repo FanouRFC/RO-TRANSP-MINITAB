38549f13af3d6cb8b8c791ebc6172f7b896a0648
unknown <fanouomega@gmail.com>
2026-04-17 22:47:42 +0300

    fix: usage of transportTable + state variable

--- src/components/Tableau.tsx
+++ src/components/Tableau.tsx
@@ -1,47 +1,49 @@
-import "../services/Minitab.ts"
-import { minitab } from "../services/Minitab.ts"
+import { useEffect, useState } from "react";
+import "../services/Minitab.ts";
+import { minitab } from "../services/Minitab.ts";
+import TableData from "./transportTable.tsx";
 
-export default function Tableau() 
-{
-    minitab();
-    return (
-        <>
-            <table>
-                <tbody>
-                    <tr>
-                        <td></td>
-                        <th>1</th>
-                        <th>2</th>
-                        <th>3</th>
-                        <th>4</th>
-                        <th>5</th>
-                    </tr>
-                    <tr>
-                        <th>A</th>
-                        <th>12</th>
-                        <th>32</th>
-                        <th>90</th>
-                        <th>99</th>
-                        <th>99</th>
-                    </tr>
-                    <tr>
-                        <th>B</th>
-                        <th>12</th>
-                        <th>32</th>
-                        <th>90</th>
-                        <th>99</th>
-                        <th>99</th>
-                    </tr>
-                    <tr>
-                        <th>C</th>
-                        <th>12</th>
-                        <th>32</th>
-                        <th>90</th>
-                        <th>99</th>
-                        <th>99</th>
-                    </tr>
-                </tbody>
-            </table>
-        </>
+export default function Tableau() {
+  var [productUnit, setProductUnit] = useState<number[][]>([
+    [24, 22, 61, 49, 83, 35],
+    [23, 39, 78, 28, 65, 42],
+    [67, 56, 92, 24, 53, 54],
+    [71, 43, 91, 67, 40, 49],
+  ]);
+
+  var [disponibleQuantity, setDisponibleQuantity] = useState<number[]>([
+    18, 32, 14, 9,
+  ]);
+  var [destinyQuantity, setDestinyQuantity] = useState<number[]>([
+    9, 11, 28, 6, 14, 5,
+  ]);
+
+  var [baseSolution, setBaseSolution] = useState<number[][]>([]);
+  var [z, setZ] = useState<number>();
+
+  useEffect(() => {
+    const resultat = minitab(
+      productUnit.map((row) => [...row]),
+      [...disponibleQuantity],
+      [...destinyQuantity],
     );
+    // console.log(" Z EST EGALE A  " + resultat.Z);
+    setBaseSolution(resultat.baseSolution);
+    setZ(resultat.Z);
+  }, [productUnit, disponibleQuantity, destinyQuantity]);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 g-10 m-5">
+      <TableData
+        data={productUnit}
+        destinyQuantity={destinyQuantity}
+        disponibleQuantity={disponibleQuantity}
+      />
+      <TableData
+        data={baseSolution}
+        destinyQuantity={destinyQuantity}
+        disponibleQuantity={disponibleQuantity}
+      />
+    </div>
+  );
 }
