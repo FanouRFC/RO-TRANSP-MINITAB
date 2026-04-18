cbe1c7f872c111903a3f740d03f4d66890034edd
unknown <fanouomega@gmail.com>
2026-04-18 21:47:04 +0300

    feat: ajout composant arc

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.css";
-import Tableau from "./components/Tableau"
+import Arc from "./components/arc";
+import Tableau from "./components/Tableau";
 
 function App() {
   return (
-    <>
+    <div className="w-full h-100">
       <p className="text-center text-4xl font-bold">
         Projet RO : TRANSP - MINITAB - STEPPING STONE
       </p>
-      <Tableau/>
-    </>
+      <Tableau />
+      <Arc />
+    </div>
   );
 }
 
