e6aa511e831e9b03f2a2485f5014ad5304760baf
unknown <fanouomega@gmail.com>
2026-04-20 19:57:19 +0300

    fix: correction props

--- src/components/arc.tsx
+++ src/components/arc.tsx
@@ -4,33 +4,25 @@ import { useEffect, useState } from "react";
 import type { Node, Edge } from "@xyflow/react";
 import { createNodes } from "../services/node";
 import { createEdge } from "../services/edge";
-import { getArcTable } from "../services/Minitab";
 import CustomEdge from "./edge";
-import type { NodeData } from "../types/arc";
+import type { EdgesData, NodeData } from "../types/arc";
 
 type ArcProps = {
   nodesData: NodeData[];
-  transportTable: number[][];
-  transportTableSolution: number[][];
+  edgesData: EdgesData[];
 };
 
-export default function Arc({
-  transportTable,
-  transportTableSolution,
-  nodesData,
-}: ArcProps) {
+export default function Arc({ edgesData, nodesData }: ArcProps) {
   const [ArcNodes, setNodes] = useState<Node[]>([]);
   const [ArcEdges, setEdges] = useState<Edge[]>([]);
+
   const edgeTypes = {
     "custom-edge": CustomEdge,
   };
 
   useEffect(() => {
-    var n = createNodes(nodesData);
-    setNodes(n);
-    var ArcsFromTable = getArcTable(transportTable, transportTableSolution);
-    var e = createEdge(ArcsFromTable);
-    setEdges(e);
+    setNodes(createNodes(nodesData));
+    setEdges(createEdge(edgesData));
   }, []);
 
   return (
