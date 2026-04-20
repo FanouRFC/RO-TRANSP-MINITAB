6c4af0824fd83ed48c3f34093389e3b52c349114
unknown <fanouomega@gmail.com>
2026-04-20 19:21:32 +0300

    données productUnit, disponibleQuantity, destinyQuantity

---
+++ src/data/table.ts
@@ -0,0 +1,8 @@
+export var productUnit = [
+    [24, 22, 61, 49, 83, 35],
+    [23, 39, 78, 28, 65, 42],
+    [67, 56, 92, 24, 53, 54],
+    [71, 43, 91, 67, 40, 49],
+]
+export var disponibleQuantity =[18, 32, 14, 9,]
+export var destinyQuantity = [9, 11, 28, 6, 14, 5]
