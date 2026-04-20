6b96cc7d5a334884150cee10fbaf1b728dee2429
unknown <fanouomega@gmail.com>
2026-04-20 19:22:36 +0300

    feat: creation types

---
+++ src/types/arc.ts
@@ -0,0 +1,13 @@
+export type NodeData = {
+  id: string;
+  label: string;
+  type: "left" | "right";
+  value?: number;
+};
+
+export type EdgesData = {
+    X: string;
+    Cxy: number;
+    Y: string;
+}
+
