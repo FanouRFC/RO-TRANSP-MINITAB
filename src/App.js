9a7f0a570bf153b2df2884de3f925ffb0cd36e5e
kxvxn_dev <128114385+KevinAndryan@users.noreply.github.com>
2026-07-04 15:55:46 +0300

    refactor: refactoring to the first state of the input

--- src/App.js
+++ src/App.js
@@ -157,36 +157,36 @@ const initialState = {
 //   zOptimal: 0,
 // };
 
-let debugState = {
-  isLoading: false,
-  isGettingStarted: false,
-  isGifDisplayed: false,
+// let debugState = {
+//   isLoading: false,
+//   isGettingStarted: false,
+//   isGifDisplayed: false,
 
-  nbLigne: 4,
-  nbColonne: 6,
+//   nbLigne: 4,
+//   nbColonne: 6,
 
-  a: [18, 32, 14, 9],
-  b: [9,11,28,6,14,5],
+//   a: [18, 32, 14, 9],
+//   b: [9,11,28,6,14,5],
 
-  isQteValid: true,
-  errorQte: false,
+//   isQteValid: true,
+//   errorQte: false,
 
-  cout: [
-    "24","22","61","49","83","35",
-    "23","39","78","28","65","42",
-    "67","56","92","24","53","54",
-    "71","43","91","67","40","49",
-  ],
+//   cout: [
+//     "24","22","61","49","83","35",
+//     "23","39","78","28","65","42",
+//     "67","56","92","24","53","54",
+//     "71","43","91","67","40","49",
+//   ],
 
-  isCoutValid: false,
+//   isCoutValid: false,
 
-  baseSolution: null,
-  finalSolution: null,
+//   baseSolution: null,
+//   finalSolution: null,
 
-  casD: false,
-  zBase: 0,
-  zOptimal: 0,
-};
+//   casD: false,
+//   zBase: 0,
+//   zOptimal: 0,
+// };
 
 
 
@@ -243,7 +243,7 @@ const reducer = (state, action) => {
       case 'reinitialiser':
         {
           console.clear();
-          return debugState;
+          return initialState;
         }
       default:
         return state;
@@ -253,7 +253,7 @@ const reducer = (state, action) => {
 
 
 const App = () => {
-  const [minitabData, dispatch] = useReducer(reducer, debugState);
+  const [minitabData, dispatch] = useReducer(reducer, initialState);
 
   return (
     <ThemeProvider theme={Theme}>
